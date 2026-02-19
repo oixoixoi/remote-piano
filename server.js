2527afa7d73da5617ac92ecb50f28fc323752efa
lhimmusic <lhimhuh@gmail.com>
2026-02-20 02:02:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /**
- * Remote Piano Server v1.1.8
- * 변경점: ms 표기 위치 변경 (SOUND 버튼 왼쪽)
+ * Remote Piano Server v1.1.9
+ * 변경점: 자기 자신에게 신호가 돌아오는 현상 차단 (Loopback 방지)
  */
 const express = require('express');
 const app = express();
@@ -14,23 +14,16 @@ app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 const rooms = {};
 
 function getRoomList() {
-    return Object.keys(rooms).map(name => ({
-        name,
-        userCount: rooms[name].users.length
-    }));
+    return Object.keys(rooms).map(name => ({ name, userCount: rooms[name].users.length }));
 }
 
 io.on('connection', (socket) => {
     socket.emit('room-list', getRoomList());
 
     socket.on('join-room', ({ roomName, userName, password }) => {
-        if (!rooms[roomName]) {
-            rooms[roomName] = { password, users: [] };
-        } else {
-            if (rooms[roomName].password !== password) {
-                return socket.emit('error-msg', '비밀번호가 일치하지 않습니다.');
-            }
-        }
+        if (!rooms[roomName]) rooms[roomName] = { password, users: [] };
+        else if (rooms[roomName].password !== password) return socket.emit('error-msg', '비밀번호가 일치하지 않습니다.');
+
         socket.join(roomName);
         const user = { id: socket.id, name: userName };
         rooms[roomName].users.push(user);
@@ -39,8 +32,12 @@ io.on('connection', (socket) => {
         io.emit('room-list', getRoomList());
     });
 
+    // 핵심 수정: 신호를 보낸 소켓(자신)을 제외한 룸의 다른 사람들에게만 전달
     socket.on('midi-msg', (data) => {
-        socket.to(data.roomName).emit('remote-midi', { id: socket.id, msg: data.msg });
+        socket.to(data.roomName).emit('remote-midi', { 
+            senderId: socket.id, // 누가 보냈는지 명시
+            msg: data.msg 
+        });
     });
 
     socket.on('ping-req', (t) => { socket.emit('ping-res', t); });
@@ -61,4 +58,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => { console.log(`🚀 v1.1.8 서버 가동 중`); });
+server.listen(PORT, () => { console.log(`🚀 v1.1.9 서버 가동 중 (무한루프 방지)`); });
